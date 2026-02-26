bcddad551b9dcf34c57d877c9eec619d8f26e806
Nicolas Abello <nabello@unab.edu.co>
2026-02-25 20:13:28 -0600

    Vista intereses.blade.php.creada

---
+++ resources/views/intereses.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mis Intereses</title>
+</head>
+<body>
+    <h1>Mis Intereses</h1>
+
+    <ul>
+        <li>Desarrollo Web</li>
+        <li>Inteligencia Artificial</li>
+        <li>Bases de Datos</li>
+        <li>Programación en PHP</li>
+        <li>Tecnología e innovación</li>
+    </ul>
+
+    <a href="/perfil">Volver al perfil</a>
+
+</body>
+</html>
