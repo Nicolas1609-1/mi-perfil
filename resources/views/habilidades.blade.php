2c427ddbbb964f70f88f0cac1a8925baa797a6a0
Nicolas Abello <nabello@unab.edu.co>
2026-02-25 20:18:01 -0600

    Vista habilidades.blade.php.creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mis Habilidades</title>
+</head>
+<body>
+    <h1>Mis Habilidades</h1>
+
+    <h3>Habilidades Técnicas</h3>
+    <ul>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>MySQL</li>
+        <li>HTML y CSS</li>
+        <li>Git y GitHub</li>
+    </ul>
+
+    <h3>Habilidades Blandas</h3>
+    <ul>
+        <li>Trabajo en equipo</li>
+        <li>Responsabilidad</li>
+        <li>Comunicación efectiva</li>
+        <li>Resolución de problemas</li>
+    </ul>
+
+    <br>
+    <a href="/perfil">Volver al perfil</a> |
+    <a href="/intereses">Ver intereses</a>
+
+</body>
+</html>
