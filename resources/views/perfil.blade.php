d0d99aad5bd255863b161523657af0eb807bae4a
Nicolas Abello <nabello@unab.edu.co>
2026-02-25 20:08:40 -0600

    Vista perfil.blade.php.creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Perfil</title>
+</head>
+<body>
+    <h1>Mi Perfil</h1>
+
+    <p><strong>Nombre:</strong> Nicolas Abello</p>
+    <p><strong>Email:</strong> nabello@unab.edu.co</p>
+    <p><strong>Carrera:</strong> Ingeniería de Sistemas</p>
+
+</body>
+</html>
