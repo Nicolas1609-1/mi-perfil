37bc315e8c92576db0911b3ca526899dd1849857
Nicolas Abello <nabello@unab.edu.co>
2026-02-25 20:21:38 -0600

    Vista metas.blade.php.creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mis Metas</title>
+</head>
+<body>
+    <h1>Mis Metas</h1>
+
+    <h3>Metas a Corto Plazo</h3>
+    <ul>
+        <li>Dominar Laravel</li>
+        <li>Mejorar mis habilidades en bases de datos</li>
+        <li>Aprender más sobre desarrollo backend</li>
+    </ul>
+
+    <h3>Metas a Mediano Plazo</h3>
+    <ul>
+        <li>Conseguir prácticas profesionales</li>
+        <li>Participar en proyectos reales de desarrollo</li>
+    </ul>
+
+    <h3>Metas a Largo Plazo</h3>
+    <ul>
+        <li>Graduarme como Ingeniero de Sistemas</li>
+        <li>Trabajar como desarrollador full stack</li>
+        <li>Crear mi propia empresa tecnológica</li>
+    </ul>
+
+    <br>
+    <a href="/perfil">Volver al perfil</a> |
+    <a href="/intereses">Intereses</a> |
+    <a href="/habilidades">Habilidades</a>
+
+</body>
+</html>
